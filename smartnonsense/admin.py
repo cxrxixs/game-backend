0a052f60086e35d708a92ac26b866643b163f5c5
cxrxixs <kurisureo@gmail.com>
2024-12-23 11:47:07 +0800

    Remove comment

--- smartnonsense/admin.py
+++ smartnonsense/admin.py
@@ -18,11 +18,6 @@ class QuestionAdminForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        """
-        if self.instance and self.instance.pk:
-            # Limit correct_answer choices to options related to this question
-            self.fields["correct_answer"].queryset = Option.objects.filter(question=self.instance)
-        """
 
 
 # Inline admin for Options (allows adding/editing options directly within the Question admin page)
@@ -53,10 +48,8 @@ class SolutionStepsInline(admin.StackedInline):
 @admin.register(Solution)
 class SolutionAdmin(SummernoteModelAdmin):
     inlines = [SolutionStepsInline]
-    # list_display = ("content", "question")
-    list_display = ("stripped_text",)
+    list_display = ("stripped_text", "question")
     search_fields = ("question__text",)  # Enables searching by question text
-    # summernote_fields = ("steps",)  # Apply Summernote to the 'steps' field
     summernote_fields = ("content",)  # Apply Summernote to the 'text' field
 
     # Custom method to display stripped text
@@ -81,21 +74,3 @@ class QuestionAdmin(SummernoteModelAdmin):
         return strip_tags(obj.text)
 
     stripped_text.short_description = "Question Text"  # Column name in admin
-
-
-# Admin for Option (if needed for standalone management)
-@admin.register(Option)
-class OptionAdmin(admin.ModelAdmin):
-    list_display = ("text", "question")
-    search_fields = ("text", "question__text")  # Enables searching by option text and question text
-    list_filter = ("question",)  # Filter options by related question
-
-
-# Admin for SolutionSteps (if needed for standalone management)
-"""
-@admin.register(SolutionStep)
-class SolutionStepsAdmin(SummernoteModelAdmin):
-    list_display = ("title", "solution", "result", "image_url")
-    search_fields = ("title", "solution__question__text")  # Enables searching by step title and question text
-    summernote_fields = ("result", "title")  # Apply Summernote to the 'result' field
-"""
