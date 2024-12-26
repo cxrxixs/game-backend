ddc887a7e401c4cf7ac4ce11d8f2d9b1fe34d213
cxrxixs <kurisureo@gmail.com>
2024-12-26 13:00:51 +0800

    Add test
    Test API response

--- api/tests.py
+++ api/tests.py
@@ -1,1 +1,18 @@
-# Create your tests here.
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+class TestApi:
+    def test_get_request_response_200(self, client):
+        url = reverse("api-questions")
+        response = client.get(url)
+
+        assert response.status_code == 200
+
+    def test_response_content(self, client):
+        url = reverse("api-questions")
+        response = client.get(url)
+
+        data = response.json()
+        assert len(data) == 2
--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("questions/", views.question_list),
+    path("questions/", views.question_list, name="api-questions"),
 ]
